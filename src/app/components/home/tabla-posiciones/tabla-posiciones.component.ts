d4537b455a1c0494fd14439b8b42dc59f7f54f4c
Byron Rosas <byron_mma@hotmail.es>
2017-09-14 16:56:01 -0500

    :D Pdf prueba angular 2

--- src/app/components/home/tabla-posiciones/tabla-posiciones.component.ts
+++ src/app/components/home/tabla-posiciones/tabla-posiciones.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as jsPDF from  'jspdf'
 @Component({
   selector: 'app-tabla-posiciones',
   templateUrl: './tabla-posiciones.component.html',
@@ -11,5 +11,15 @@ export class TablaPosicionesComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // You'll need to make your image into a Data URL
+    // Use http://dataurl.net/#dataurlmaker    
+    a(){
+      let doc = new jsPDF()
+      
+      doc.setFontSize(40)
+      doc.fromHTML('<h1>Hola</h1>',35, 25) 
+      doc.save('file.pdf');     
+    }    
 
 }
