09bb655e642a5d853d8742681ee2a260ff67c446
darwin <darwinj94@hotmail.com>
2017-09-29 23:24:14 -0500

    jugadores component tocado

--- src/app/services/personal.service.ts
+++ src/app/services/personal.service.ts
@@ -13,7 +13,29 @@ export class PersonalService {
     this.url=GLOBAL.url;
   }
 
-
+  addExcelPersonal( url: string, files:Array<File>,token: string,name:string){   
+    return new Promise(function(resolve,reject) {
+      //simular el comportamiento de un formulario.
+      var formData : any = new FormData();
+      var xhr = new XMLHttpRequest();
+      //recorrer los ficheros que hay en la base de datos
+      if(files!=undefined){
+        for(var i = 0;i < files.length;i++) {
+            formData.append(name, files[i], files[i].name);
+        }
+      }
+      
+      xhr.onreadystatechange = function() {
+          if(xhr.readyState == 4) {
+              if(xhr.status ==200) { resolve(JSON.parse(xhr.response)); } 
+              else { reject(xhr.response); }
+          }
+      }
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('Authorization', token);
+      xhr.send(formData);
+      }); 
+  }
   //Nueva
   addPersonal( url: string, params: Personal, files:Array<File>,token: string,name:string){   
     return new Promise(function(resolve,reject) {
